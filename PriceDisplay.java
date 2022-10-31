05b7d6b0cdd08b6dca97da196dba08b910e9204b
archiemartini <archie.cgm@gmail.com>
2022-10-31 15:07:14 +0000

    Calculator, Clock, Hello, PriceDisplay class prac

---
+++ PriceDisplay.java
@@ -0,0 +1,9 @@
+class PriceDisplay {
+
+  public static void main(String[] args) {
+    String itemName = "Coffee machine";
+    Double price = 15.99;
+
+    System.out.printf("The price of the item %s is %.2f \n", itemName, price);
+  }
+}
